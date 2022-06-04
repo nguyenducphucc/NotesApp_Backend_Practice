1a2a2bfb95ba5324cfed535c3c0783c554e7ffc8
nguyenducphucc <jonybounce@gmail.com>
2022-06-04 15:53:41 -0500

    uibuild

--- controllers/login.js
+++ controllers/login.js
@@ -25,7 +25,9 @@ loginRouter.post("/", async (req, res) => {
 
   const token = jwt.sign(userForToken, config.SECRET, { expiresIn: 60 * 60 });
 
-  res.status(200).send({ token, username: user.username, name: user.name });
+  return res
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = loginRouter;
